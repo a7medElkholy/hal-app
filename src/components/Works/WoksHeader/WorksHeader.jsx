81ab53cfeb40105414fb11126296cf7ae6616a52
a7medElkholy <a7medeelkholy@gmail.com>
2025-10-31 06:13:27 +0200

    finish resolves

--- src/components/Works/WoksHeader/WorksHeader.jsx
+++ src/components/Works/WoksHeader/WorksHeader.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 export default function WorksHeader() {
   return (
-    <header className="bg-gradient-to-b from-[#032345] to-[#3A5973] h-screen flex justify-center items-center">
+    <header
+     style={{
+    background: "linear-gradient(to bottom, #032345, #3A5973)",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }}
+     >
       <div>
         <h1 className="text-white font-extrabold text-7xl">أعمالنا </h1>
       </div>
